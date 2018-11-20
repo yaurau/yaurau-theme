464eaaefe16054e953639bce5c83c33f1ce0fda1
yaurau <rama7777@bk.ru>
2018-11-20 10:42:02 +0300

    Code update 20/11_1

--- footer.php
+++ footer.php
@@ -15,13 +15,6 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'yaurau-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'yaurau-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'yaurau-theme' ), 'yaurau-theme', '<a href="https://github.com/yaurau">Rauvtovich Yauhen</a>' );
