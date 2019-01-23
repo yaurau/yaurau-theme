a92d1e8e83c4eeea1d7fe039759c7e285745e1e1
yaurau <rama7777@bk.ru>
2019-01-23 15:31:56 +0300

    Code update 23/01_2

--- header.php
+++ header.php
@@ -10,11 +10,49 @@
  */
 
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="apple-touch-icon" sizes="180x180" href="https://ravtovich.by/wp-content/uploads/2018/12/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://ravtovich.by/wp-content/uploads/2018/12/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://ravtovich.by/wp-content/uploads/2018/12/favicon-16x16.png">
+    <title>Разработка сайтов под ключ </title>
+	<meta name="description"  content="Разработка сайтов под ключ. Специализируемся на Landing Page и корпоративных сайтах. Делаем недорого и c уникальным дизайном" />
+<meta name="keywords"  content="pазработка, Landing, Page, ключ, недорого, уникальным, дизайном, сайт" />
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Разработка сайтов под ключ">
+    <meta property="og:title" content="Разработка сайтов под ключ">
+    <meta property="og:description" content="Разработка сайтов под ключ. Специализируемся на Landing Page и корпоративных сайтах. Делаем недорого и c уникальным дизайном">
+    <meta property="og:url" content="https://ravtovich.by">
+    <meta property="og:locale" content="ru_RU">
+    <meta property="og:image" content="https://ravtovich.by/wp-content/uploads/2019/01/open-graph.jpg">
+    <meta property="og:image:width" content="968">
+    <meta property="og:image:height" content="504">
+    <meta name="twitter:title" content="Разработка сайтов под ключ">
+    <meta name="twitter:description" content="Разработка сайтов под ключ. Специализируемся на Landing Page и корпоративных сайтах. Делаем недорого и c уникальным дизайном">
+    <meta name="twitter:image:src" content="https://ravtovich.by/wp-content/uploads/2019/01/open-graph.jpg">
+    <meta name="twitter:url" content="https://ravtovich.by">
+    <meta name="twitter:domain" content="ravtovich.by">
+    <meta name="yandex-verification" content="3bc167ce13b9a15e" />
+
+    <!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(51606710, "init", {
+        id:51606710,
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/51606710" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
