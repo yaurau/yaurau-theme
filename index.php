18eec9582c15c7c548d8b5ebb12d93170571e544
yaurau <rama7777@bk.ru>
2019-02-15 15:26:27 +0300

    Code update 15/02

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 get_header();
 ?>
-
+    <div class="container">
+        <h1><?php echo get_bloginfo('blogname') ?></h1>
+        <h2><?php echo get_bloginfo('description') ?></h2>
+    </div>
 <?php
 get_footer();
