a5f1d3d7bf3207c5036780aef947b2d104399d1e
yaurau <rama7777@bk.ru>
2019-02-19 08:20:37 +0300

    Code update 19/02_1

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
+
 <?php
 get_header();
 ?>
+    <header id="header" class="header">
     <div class="container">
         <div class="row">
+            <!--Site name-->
             <h1><?php echo get_bloginfo('blogname') ?></h1>
         </div>
         <div class="row">
+            <!--Brief description-->
             <h2><?php echo get_bloginfo('description') ?></h2>
         </div>
     </div>
+    </header>
+    <section id="one" class="one">
+        <div class="container">
+            <div class="row">
+                <!--Our text-->
+            </div>
+        </div>
+    </section>
 <?php
 get_footer();
