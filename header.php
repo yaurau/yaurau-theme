b906a39e0a51dc3f213487d9b9fb66324e13b3ad
yaurau <rama7777@bk.ru>
2019-02-28 16:23:25 +0300

    Code update 28/02

--- header.php
+++ header.php
@@ -1,6 +1,3 @@
-<?php
-
-?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -9,19 +6,19 @@
 	<link rel="apple-touch-icon" sizes="180x180" href="https://ravtovich.by/wp-content/uploads/2018/12/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="https://ravtovich.by/wp-content/uploads/2018/12/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="https://ravtovich.by/wp-content/uploads/2018/12/favicon-16x16.png">
-    <title>Разработка сайтов под ключ </title>
+    <title><?php echo get_bloginfo('blogname') ?> </title>
 	<meta name="description"  content="Разработка сайтов под ключ. Специализируемся на Landing Page и корпоративных сайтах. Делаем недорого и c уникальным дизайном" />
 <meta name="keywords"  content="pазработка, Landing, Page, ключ, недорого, уникальным, дизайном, сайт" />
     <meta property="og:type" content="website">
-    <meta property="og:site_name" content="Разработка сайтов под ключ">
-    <meta property="og:title" content="Разработка сайтов под ключ">
+    <meta property="og:site_name" content="<?php echo get_bloginfo('blogname') ?>">
+    <meta property="og:title" content="<?php echo get_bloginfo('blogname') ?>">
     <meta property="og:description" content="Разработка сайтов под ключ. Специализируемся на Landing Page и корпоративных сайтах. Делаем недорого и c уникальным дизайном">
     <meta property="og:url" content="https://ravtovich.by">
     <meta property="og:locale" content="ru_RU">
     <meta property="og:image" content="https://ravtovich.by/wp-content/uploads/2019/01/open-graph.jpg">
     <meta property="og:image:width" content="968">
     <meta property="og:image:height" content="504">
-    <meta name="twitter:title" content="Разработка сайтов под ключ">
+    <meta name="twitter:title" content="<?php echo get_bloginfo('blogname') ?>">
     <meta name="twitter:description" content="Разработка сайтов под ключ. Специализируемся на Landing Page и корпоративных сайтах. Делаем недорого и c уникальным дизайном">
     <meta name="twitter:image:src" content="https://ravtovich.by/wp-content/uploads/2019/01/open-graph.jpg">
     <meta name="twitter:url" content="https://ravtovich.by">
