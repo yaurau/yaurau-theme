ff6e230541384058fb724128ae1f5248c03e9f92
yaurau <rama7777@bk.ru>
2019-03-11 22:56:25 +0300

    Code update 11/03

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,40 @@
  */
 
 const gulp = require('gulp');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+
+
+const jsFiles = [
+    './js/**/*.js'
+];
+const cssFiles = [
+    '.node_modules/normalize.css/normalize.css',
+    './css/**/*.css'
+];
+
+function styles() {
+    return gulp.src(cssFiles)
+        .pipe(concat('yaurau_theme.css'))
+        .pipe(autoprefixer({
+            browsers:[' >0.1% '],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./css'));
+}
+
+function script() {
+    return gulp.src(jsFiles)
+        .pipe(concat('yaurau_theme.js'))
+        .pipe(gulp.dest('./js'));
+
+}
+
+function watch() {
+    gulp.watch('./src/css/**/*.css');
+    gulp.watch('./src/js/**/*.js');
+}
+
+gulp.task('styles', styles);
+gulp.task('script', script);
+gulp.task('watch', watch);
